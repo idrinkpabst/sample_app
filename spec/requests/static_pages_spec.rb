040474849de6b1206506b997ecfd42c1ab6e34bf
Jake Parks <jakob.parks@gmail.com>
2012-08-17 10:47:11 -0700

    fix merge

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,6 @@ describe "Static pages" do
   subject { page }
 
   describe "Home page" do
-<<<<<<< HEAD
 
     before { visit root_path }
 
@@ -13,23 +12,6 @@ describe "Static pages" do
     it { should have_selector('title', 
                       text: full_title('')) }
     it { should_not have_selector('title', text: '| Home') }
-
-=======
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Sample App')
-    end 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', 
-                      text: 'Ruby on Rails Tutorial Sample App')     
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', text: '| Home')
-    end
->>>>>>> master
   end
 
   describe "Help page" do
